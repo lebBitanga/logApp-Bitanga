54c6de33ecd07d1b8f6d4eaa4e213ceaf98de32d
lebBitanga <201980321@psu.palawan.edu.ph>
2022-03-17 16:53:04 +0800

    BitangaLorwin

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+	define('ROOT_URL', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6479398');
+	define('DB_PASS', 'TpAEcHKYeF');
+	define('DB_NAME', 'sql6479398');
+?>
